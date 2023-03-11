b22389aac581ee4009aae2ec9f50be37610d6965
LIMA <fausto.lima@thomsonreuters.com>
2023-03-11 11:10:01 -0300

    Iniciando o repositorio para controle de versão do projeto DiarioDeBordo

---
+++ js/filtra.js
@@ -0,0 +1,27 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function(){
+	console.log(this.value);
+	var registros = document.querySelectorAll(".registro");
+	
+	if( this.value.length > 0){
+		for( var i = 0; i < registros.length ; i++){
+			var registro = registros[i];
+			var tdNome = registro.querySelector(".info-nome");
+			var nome = tdNome.textContent;
+			var expressao = new RegExp(this.value,"i");
+
+			if(!expressao.test(nome)){
+				registro.classList.add("invisivel");
+			}else{
+				registro.classList.remove("invisivel");
+			}
+		}
+	}else{
+		for (var i = 0; i < registros.length; i++) {
+			var registro = registros[i];
+			registro.classList.remove("invisivel");
+		}
+	}
+
+});
---
+++ js/form.js
@@ -0,0 +1,111 @@
+var botaoAdicionar = document.querySelector("#adicionar-registro");
+		botaoAdicionar.addEventListener("click", function(){    
+			event.preventDefault();                             
+
+			var form = document.querySelector("#form-adiciona") 
+			var regristro = obtemRegistroDoFormulario(form);
+
+			adicionaRegistroNaTabela(regristro);
+			
+			form.reset();
+			var mensagensErro = document.querySelector("#mensagens-erro");
+			mensagensErro.innerHTML = "Registro Adicionado com sucesso";
+});
+
+		function adicionaRegistroNaTabela(regristro){
+			var registroTr = montaTr(regristro);
+			var tabela = document.querySelector("#tabela-registros");		
+			var registroStatus = registroTr.querySelector(".info-status")
+			
+			tabela.appendChild(registroTr);
+
+			if (registroStatus.textContent == "Concluido") {
+				registroStatus.setAttribute('class','colorG')
+			}
+			if (registroStatus.textContent == "Pendente") {
+				registroStatus.setAttribute('class','colorR')
+			}			
+			if (registroStatus.textContent == "Aguardando Coordenador") {
+				registroStatus.setAttribute('class','colorB')
+			}			
+			if (registroStatus.textContent == "Aguardadno Desenvolvimento" || registroStatus.textContent == "Aguardadno GP") {
+				registroStatus.setAttribute('class','colorO')
+			}
+
+		}
+
+		function exibeMensagensDeErro(erros){
+			var ul = document.querySelector("#mensagens-erro");
+			ul.innerHTML = "";
+			erros.forEach(function(erro){
+				var li = document.createElement("li");
+				li.textContent = erro;
+				ul.appendChild(li);
+			});
+		}
+
+		function obtemRegistroDoFormulario(form){				
+			var regristro = {
+				nome: form.nome.value,
+				assunto: form.assunto.value,
+				comentario: form.comentario.value,
+				status: form.status.value,	
+			}			
+			return regristro;
+		}
+
+		function montaTr(registro){
+			var pacienteTr = document.createElement("tr"); 
+			pacienteTr.classList.add("registro");
+	
+			pacienteTr.appendChild(montaTd(registro.nome, "info-nome"));                     
+			pacienteTr.appendChild(montaTd(registro.assunto, "info-assunto"));
+			pacienteTr.appendChild(montaTd(registro.comentario, "info-comentario"));
+			pacienteTr.appendChild(montaTd(registro.status, "info-status"));
+			pacienteTr.appendChild(montaTd(dataHora, "info-data"));
+
+			var data = new Date()
+			const dia = String(data.getDate()).padStart(2, '0')
+			const mes = String(data.getMonth()+1).padStart(2, '0')
+			const ano = data.getFullYear()
+			const dataAtual = `${dia}/${mes}/${ano}`
+	
+			const horas = String(data.getHours()).padStart(2, '0')
+			const minutos = String(data.getMinutes()).padStart(2, '0')
+			const segundos = String(data.getSeconds()).padStart(2, '0')
+			const horaAtual = `${horas}:${minutos}:${segundos}`
+	
+			dataHora = `${dataAtual} - ${horaAtual}`
+
+			return pacienteTr;	
+		}
+
+		function montaTd(dado,classe){
+			var td = document.createElement("td");
+			td.textContent = dado;
+			td.classList.add(classe);
+			return td;
+		}
+
+			var data = new Date()
+			const dia = String(data.getDate()).padStart(2, '0')
+			const mes = String(data.getMonth()+1).padStart(2, '0')
+			const ano = data.getFullYear()
+			const dataAtual = `${dia}/${mes}/${ano}`
+	
+			const horas = String(data.getHours()).padStart(2, '0')
+			const minutos = String(data.getMinutes()).padStart(2, '0')
+			const segundos = String(data.getSeconds()).padStart(2, '0')
+			const horaAtual = `${horas}:${minutos}:${segundos}`
+	
+			dataHora = `${dataAtual} - ${horaAtual}`
+		
+		
+	
+
+		
+
+		
+		
+		
+	
